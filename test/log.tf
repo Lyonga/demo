39bdab95611b0b5634b97e3ab234f36df968b9bb
Lyonga <60431189+Lyonga@users.noreply.github.com>
2025-01-23 08:25:04 -0500

    Update log.tf

--- test/log.tf
+++ test/log.tf
@@ -1,23 +1,7 @@
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "${path.module}/code/lambda_function.py"
-  output_path = "${path.module}/lambda_function.zip"
-}
+[ERROR] KeyError: '2024-04-01'
+Traceback (most recent call last):
+  File "/var/task/lambda_function.py", line 769, in lambda_handler
+    mainDailyDict = process_costchanges_per_day(mainCostDict)
+  File "/var/task/lambda_function.py", line 182, in process_costchanges_per_day
+    reportCostDict[dayCost['TimePeriod']['Start']].update(
 
-resource "aws_lambda_function" "cost_alert_lambda" {
-  filename         = data.archive_file.lambda_zip.output_path
-  function_name    = local.lambda_cost
-  role             = aws_iam_role.lambda_exec_role.arn
-  handler          = "lambda_function.lambda_handler"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime          = "python3.9"
-  memory_size      = 1024
-  timeout          = 300
-  environment {
-    variables = {
-      RECIPIENT   = var.recipient
-      SENDER = var.sender
-    }
-  }
-  tags        = var.tags
-}
