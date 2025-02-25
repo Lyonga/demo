cc5ee06bc5f03f513948964580631a58849c0846
Lyonga <60431189+Lyonga@users.noreply.github.com>
2025-02-25 09:05:28 -0500

    Delete test/log.tf

--- test/log.tf
+++
@@ -1,7 +0,0 @@
-[ERROR] KeyError: '2024-04-01'
-Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 769, in lambda_handler
-    mainDailyDict = process_costchanges_per_day(mainCostDict)
-  File "/var/task/lambda_function.py", line 182, in process_costchanges_per_day
-    reportCostDict[dayCost['TimePeriod']['Start']].update(
-
